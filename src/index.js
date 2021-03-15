2ba87ef5c9e9c771cb619152575dc6409ffcad79
Shpakouski <job.shpakouski@gmail.com>
2021-03-16 00:23:18 +0300

    task is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = bracketsConfig.map(b => b.join(""));
+
+    for (let i = 0; i < config.length; i++) {
+        if (str.includes(config[i])) {
+            str = str.replace(config[i], "");
+
+            i = -1;
+        }
+    }
+
+    return str === '';
 }
